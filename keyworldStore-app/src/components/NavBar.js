e3d553f8bba8090e313904bd73afca1607009145
ernestomolinaj <ernestomolinaj@gmail.com>
2022-05-24 01:36:45 -0300

    Desafío: Menu e-commerce -Se agregó el componente navbar y se lo incluyó desde App.js -Se creo una hoja de estilos .css y se elimino la que viene por default

---
+++ keyworldStore-app/src/components/NavBar.js
@@ -0,0 +1,13 @@
+export const NavBar = () => {
+    return <header className="navbar">
+        
+        <h1>Keyworld</h1>
+        
+        <nav>
+            <a>Teclados</a>
+            <a>Accesorios</a>
+            <a>Sets</a>
+        </nav>
+
+    </header>
+}
