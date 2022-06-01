7623a35ad972d4b26674a22c230a6ef637940d10
ernestomolinaj <ernestomolinaj@gmail.com>
2022-06-01 04:40:39 -0300

    Comment cleaning Added some sugar on the item count component

--- keyworldStore-app/src/components/ItemCount/ItemCount.js
+++ keyworldStore-app/src/components/ItemCount/ItemCount.js
@@ -1,40 +1,27 @@
 import { useState, useEffect } from "react"
 
-export const ItemCount = ( { itemStock }) => {
-    
+export const ItemCount = ({ itemStock }) => {
+
     const [itemAmmountSelected, setItemAmmountSelected] = useState(0)
     const [decreaseItemAmmountButtonIsShown, setDecreaseItemAmmountButtonIsShown] = useState(false)
     const [increaseItemAmmountButtonIsShown, setIncreaseItemAmmountButtonIsShown] = useState(true)
 
-    const changeItemAmmountSelected = (ammount) =>
-    {
-        if (itemAmmountSelected + ammount < 0 || itemAmmountSelected + ammount > itemStock)
+    const changeItemAmmountSelected = (ammount) => {
+        (itemAmmountSelected + ammount < 0 || itemAmmountSelected + ammount <= itemStock) ?
+            setItemAmmountSelected(itemAmmountSelected + ammount) :
             console.log("There aren't enough items in stock or trying to decrease item ammount bellow 0.");
-        else
-            setItemAmmountSelected(itemAmmountSelected + ammount);
-        // itemAmmountSelected + ammount >= 0 || itemAmmountSelected + ammount > 10  ? setItemAmmountSelected(itemAmmountSelected + ammount) : console.log("There aren't enough items in stock or trying to decrease item ammount bellow 0.")  
     }
 
-    const showDecreaseItemAmmountButton = () => setDecreaseItemAmmountButtonIsShown(!decreaseItemAmmountButtonIsShown)
-    const showIncreaseItemAmmountButton = () => setIncreaseItemAmmountButtonIsShown(!increaseItemAmmountButtonIsShown)
-
-    useEffect(() => {  
-        if (itemAmmountSelected == 0) setDecreaseItemAmmountButtonIsShown(false)
-        else setDecreaseItemAmmountButtonIsShown(true);
-        if (itemAmmountSelected == itemStock) setIncreaseItemAmmountButtonIsShown(false)
-        else setIncreaseItemAmmountButtonIsShown(true); 
-        console.log(itemAmmountSelected)
+    useEffect(() => {
+        itemAmmountSelected == 0 ? setDecreaseItemAmmountButtonIsShown(false) : setDecreaseItemAmmountButtonIsShown(true);
+        (itemAmmountSelected == itemStock) ? setIncreaseItemAmmountButtonIsShown(false) : setIncreaseItemAmmountButtonIsShown(true);
     }, [itemAmmountSelected])
 
-    return(
+    return (
         <div>
-            {
-                decreaseItemAmmountButtonIsShown ? <button onClick={() => changeItemAmmountSelected(-1)}>-</button> : <div/>
-            }
-            &nbsp; { itemAmmountSelected } &nbsp;
-            {
-                increaseItemAmmountButtonIsShown ? <button onClick={() => changeItemAmmountSelected(1)}>+</button> : <div/>
-            }
+            {decreaseItemAmmountButtonIsShown ? <button onClick={() => changeItemAmmountSelected(-1)}>-</button> : <div />}
+            &nbsp; {itemAmmountSelected} &nbsp;
+            {increaseItemAmmountButtonIsShown ? <button onClick={() => changeItemAmmountSelected(1)}>+</button> : <div />}
         </div>
     )
 }
