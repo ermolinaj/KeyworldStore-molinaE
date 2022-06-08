eb23f09c56513a5ec9879102af32654e2a5c9812
ernestomolinaj <ernestomolinaj@gmail.com>
2022-06-08 16:00:51 -0300

    Correcciones commit

--- keyworldStore-app/src/components/ItemDetailCOntainer/ItemDetailContainer.js
+++ keyworldStore-app/src/components/ItemDetailCOntainer/ItemDetailContainer.js
@@ -1,31 +1,34 @@
-import {Fragment, useEffect, useState} from "react"
-import './Item.css'
+import {useEffect, useState} from "react"
+import ItemDetail from "../ItemDetail/ItemDetail"
+import {obtainProduct} from '../../mocks/DataAccess.js'
 
-const Item = ({item}) => {
 
+const ItemDetailContainer = ({itemId}) => {
+    const [item, setItem] = useState([])
     const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        setLoading(true)
+
+        obtainProduct(itemId)
+            .then((response) => {
+                setItem(response)
+            })
+            .catch((error) => {
+                console.log('ERROR: ', error)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [])
+
+    console.log(item)
+
     return (
-        <Fragment key={item.id}>
-            loading
-                ? <span>Cargando Productos...</span>
-                : <ItemList itemsIds={itemIds}/>
-            <h2 className="item__name">{item.name}</h2>
-            <img src={"images/"+ item.imageName} className="item__image" alt="" />
-            <p className="item__description">{item.description}</p>
-            <dl>
-                {/* TODO: create a component for the description */}
-                { item.productInformation.map(pi => (
-                    <>
-                        <dt> { pi.key} </dt>
-                        <dd> { pi.value } </dd>
-                    </> 
-                ))}
-            </dl>
-            <p className="item__price">${item.price}</p>
-            <button className="item__addToCartButton">Agregar al Carrito</button>
-        </Fragment>
+        loading
+            ? <p>Cargando Detalles del Producto...</p>
+            : <ItemDetail item={item}/>
     )
 }
 
-export default Item
+export default ItemDetailContainer
--- keyworldStore-app/src/components/ItemList/ItemList.js
+++ keyworldStore-app/src/components/ItemList/ItemList.js
@@ -1,11 +1,9 @@
-import { useState } from "react"
 import ItemDetailContainer from "../ItemDetailContainer/ItemDetailContainer"
 import {obtainAllProductIds} from '../../mocks/DataAccess.js'
 
 const ItemList = () => {
 
     const itemIds = obtainAllProductIds()
-    console.log(itemIds)
 
     return (
         <div>
